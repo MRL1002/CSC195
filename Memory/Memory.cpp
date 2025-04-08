6b1234d80f5e5233af0cba2989f3468c6e3d0101
Izzy :3 <8mrl1002@gmail.com>
2025-04-08 16:09:39 -0600

    updated notes

--- Memory/Memory.cpp
+++ Memory/Memory.cpp
@@ -2,8 +2,18 @@
 #include <iostream>
 using namespace std;
 
-void set(int i) {
+// this method passes the argument by reference
+void set(int& i) {
     i = 100;
+}
+
+void swap(int* i1, int* i2) {
+
+    int intTemp = *i1;
+
+    *i1 = *i2;
+    *i2 = intTemp;
+
 }
 
 int main()
@@ -11,11 +21,27 @@ int main()
     int i1 = 10;
     int i2 = 20;
 
-    int& r = i1; // r is now a reference to i1
+    int& r = i1; // r is now a reference to i1, basically like an ailius for it
 
-    //set(i1);
+    int* p; // pointers can be uninitialized, refs can't
+            // null pointer is the default typically
+
+    p = &i1;
+
     r = 45; // so now, i1 is now 45
 
-    cout << i1 << endl;
+    //set(i1);
+
+    swap(&i1, &i2); // swaps the values, we need the ADDRESS to i1 and i2 to be the arguments, not just the values
+
+    cout << "i1: " << i1 << endl;
+    cout << "i2: " << i2 << endl;
+    cout << endl;
+    cout << "i1 Address: " << &i1 << endl; // prints memeory address of i1
+    cout << "i1 Reference Address: " << &r << endl;  // pritns address of r, will be the same as above, because it's basically a redirector
+    cout << endl;
+    cout << "Value stored in p: " << *p << endl;  // will retrieve the val stored in the address stored in p / "dereferece" p
+    cout << "address of p: " << &p << endl;  // will retrieve the address of p
+    cout << "address STORED IN p: " << p << endl;  // will retrieve the address STORED IN p, or address of i1
 
 }
