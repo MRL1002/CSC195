b9aeee46543c4d247702c6b2e1eedac294d43663
Izzy :3 <8mrl1002@gmail.com>
2025-04-24 16:18:14 -0600

    started working on fractions lab

--- FractionsAssignment/Fraction.cpp
+++ FractionsAssignment/Fraction.cpp
@@ -1,1 +1,37 @@
 #include "Fraction.h"
+
+namespace mathlib {
+	template<typename T>
+	Fraction<T>& Fraction<T>::operator+(Fraction<T>& f2)
+	{
+		Fracton result;
+		result.num = num + f2.num;
+		result.denom = denom + f2.denom;
+	}
+
+	template<typename T>
+	Fraction<T>& Fraction<T>::operator-(Fraction<T>& f2)
+	{
+		Fracton result;
+		result.num = num - f2.num;
+		result.denom = denom - f2.denom;
+	}
+
+	template<typename T>
+	Fraction<T>& Fraction<T>::operator*(Fraction<T>& f2)
+	{
+		Fracton result;
+		result.num = num * f2.num;
+		result.denom = denom * f2.denom;
+	}
+
+	template<typename T>
+	Fraction<T>& Fraction<T>::operator/(Fraction<T>& f2)
+	{
+		Fracton result;
+		result.num = num / f2.num;
+		result.denom = denom / f2.denom;
+	}
+
+
+}
--- FractionsAssignment/Fraction.h
+++ FractionsAssignment/Fraction.h
@@ -1,12 +1,21 @@
-#pragma once
+#pragma once  
+#include <iostream>  
 
-template<typename T>
-class Fraction
-{
-public:
-	Fraction(T num, T denom) { if denom = 0: denom = 1; }
+namespace mathlib {
+    template<typename T>
+    class Fraction {
 
+    private:
+        T num, denom;
 
+    public:
+        Fraction(T num, T denom) : num(num), denom(denom) {
+            if (denom == 0) this->denom = 1;
+        }
 
-};
-
+        Fraction<T>& operator+(Fraction<T>& f2);
+        Fraction<T>& operator-(Fraction<T>& f2);
+        Fraction<T>& operator*(Fraction<T>& f2);
+        Fraction<T>& operator/(Fraction<T>& f2);
+    };
+}
